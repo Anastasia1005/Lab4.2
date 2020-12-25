967010c02097671b3d1948331f4f64b94bb0cf4e
Magiske <nastya.borzenko10@mail.ru>
2020-12-25 22:11:35 +0700

    финальная ветка

--- Lab4.2_TeamWork/Program.cs
+++ Lab4.2_TeamWork/Program.cs
@@ -10,16 +10,38 @@ namespace Lab4._2_TeamWork
     {
         static void Main(string[] args)
         {
-            int A = 0, B = 0, choice;
+            for (; ; )
+            {
+                Console.Write("Введите номер пункта меню (или другое число, если хотите выйти): ");
+                choice = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("1. Ввести число A");
-            Console.WriteLine("2. Ввести число B");
-            Console.WriteLine("3. Сложить A и B");
-            Console.WriteLine("4. Вычесть B из A");
-            Console.WriteLine("5. Умножить A на B");
-            Console.WriteLine("6. Разделить A на B");
-
-            Console.ReadKey();
+                if (choice == 1)
+                {
+                    A = Enter_A();
+                }
+                else if (choice == 2)
+                {
+                    B = Enter_B();
+                }
+                else if (choice == 3)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат сложения = {2}", A, B, A + B);
+                }
+                else if (choice == 4)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат вычитания B из A = {2}", A, B, A - B);
+                }
+                else if (choice == 5)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат умножения = {2}", A, B, A * B);
+                }
+                else if (choice == 6)
+                {
+                    Console.WriteLine("Число A = {0}, число B = {1}, результат деления A на B = {2}", A, B, A / B);
+                }
+                else
+                    break;
+            }
         }
     }
 }
