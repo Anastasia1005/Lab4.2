adb16a064e451cd80916cf13ad52ce9f3216f72c
Magiske <nastya.borzenko10@mail.ru>
2020-12-25 12:27:37 +0700

    первый пошёл

---
+++ Lab4.2_TeamWork/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4._2_TeamWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0, B = 0, choice;
+
+            Console.WriteLine("1. Ввести число A");
+            Console.WriteLine("2. Ввести число B");
+            Console.WriteLine("3. Сложить A и B");
+            Console.WriteLine("4. Вычесть B из A");
+            Console.WriteLine("5. Умножить A на B");
+            Console.WriteLine("6. Разделить A на B");
+
+            Console.ReadKey();
+        }
+    }
+}
